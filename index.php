0e6193428d4f859dff6d6741234c43466cd792e7
Axel Segundo <axeljasegundo@gmail.com>
2020-08-05 13:00:39 -0500

    Cambios login

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
-<?php session_start();
-
-if (isset($_SESSION['usuario'])) {
-    header('Location: contenido.php');
-}else {
-    header('Location: login.php');
-}
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/interfaz_login.css">
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <div class="logo">
+                <img src="imagenes/logo.jpg" alt="">
+            </div>
+            <div class="logo2">
+                <img src="imagenes/Tlax.png" alt="">
+            </div>
+        </div>
+    </header>
+    <div class="contenedor-form">
+        <form action="login.php" method="POST" class="formulario" name="login">
+            <div class="forma">
+				<i class="icono izquierda fa fa-user"></i>  <input type="text" name="usuario" class="usuario" placeholder="Usuario">
+            </div>
+            <div class="forma">
+				<i class="icono izquierda fa fa-lock"></i>  <input type="password" name="password" class="password_btn" placeholder="Contraseña">
+				<i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+			</div>
+        </form>
+        <div class="contenedor-inv">
+        <div class="alumno">
+            <p>Si usted no cuenta con un usuario, favor de ingresar aqui</p></br>
+            <a href="contenido_alumno.php"class="uv">Usuario invitado</a>
+        </div>
+    </div>
+    </div>
+    <?php require 'views/footer.php'; ?>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,38 @@
-<?php session_start();
-//include("Funciones/BDI.php");
-
-
-require 'views/login.view.php';
+<?php
+  session_start();
+  
+  // Obtengo los datos cargados en el formulario de login.
+  $usuario = $_POST['usuario'];
+  $password = $_POST['password'];
+  
+  // Datos para conectar a la base de datos.
+  $nombreServidor = "localhost";
+  $nombreUsuario = "root";
+  $passwordBaseDeDatos = "";
+  $nombreBaseDeDatos = "integradora";
+ 
+  // Crear conexión con la base de datos.
+  $conn = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
+  
+  // Validar la conexión de base de datos.
+  if ($conn ->connect_error) {
+    die("Connection failed: " . $conn ->connect_error);
+  }
+  
+  // Consulta segura para evitar inyecciones SQL.
+  $sql = "SELECT * FROM usuarios WHERE usuario=':usuario' AND password = ':password'";
+  $resultado = $conn->query($sql);
+  
+  // Verificando si el usuario existe en la base de datos.
+  if($resultado){
+	// Guardo en la sesión el email del usuario.
+	$_SESSION['usuario'] = $usuario;
+	
+	// Redirecciono al usuario a la página principal del sitio.
+	header("HTTP/1.1 302 Moved Temporarily"); 
+    header("Location: contenido_profesor.php"); 
+  }else{
+	echo 'El email o password es incorrecto, <a href="index.php">vuelva a intenarlo</a>.<br/>';
+  }
 
 ?>
