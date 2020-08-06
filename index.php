48bc1729052591b7238d9d884871f53b3e345b21
Diego-bit <ugartediegoedgar@gmail.com>
2020-08-05 21:59:12 -0500

    actualizacion

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/general.css">
-    <link rel="stylesheet" href="css/log.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Login</title>
 </head>
 
