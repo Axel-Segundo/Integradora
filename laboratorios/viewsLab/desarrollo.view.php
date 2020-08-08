ec7492d4a5813b4dd263eddf1c90fcff15a61e24
Axel Segundo <axeljasegundo@gmail.com>
2020-08-08 14:41:04 -0500

    seguridad en labs

---
+++ laboratorios/viewsLab/desarrollo.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio de Desarrollo</title>
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/inicio_profesor.css">
+</head>
+<body>
+    <header>
+        <?php require "headerprof.php" ?>
+    </header>
+
+    <footer>
+        <?php require "../views/footer.php" ?>
+    </footer>
+</body>
+</html>
