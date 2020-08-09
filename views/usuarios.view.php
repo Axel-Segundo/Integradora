26563f2b3768de2e8d28bf041fe92f476589f026
Diego-bit <ugartediegoedgar@gmail.com>
2020-08-08 22:23:42 -0500

    creacion de archivos para crear usuarios

---
+++ views/usuarios.view.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/usuarios.css">
+    <title>Usuario</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="contenedor">
+            <div class="logo">
+                <img src="../imagenes/logo2.jpg" alt="">
+            </div>
+
+            <div class="menu">
+                <ul class="menup">
+                    <li>
+                        <div>
+                            <a href="" class="perfil">
+                                <p> Perfil</p>
+                                <div><img src="../avatar/defect.jpg" alt=""></div>
+                            </a>
+                        </div>
+                        <ul class="subm">
+                            <li><a href="">Configuracion</a></li>
+                            <li><a href="">Cerrar Sesion</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+    </header>
+
+    <section class="main">
+        <!--Todo el codigo de consulta de los usuarios -->
+
+
+
+        <a href="" class="crear_usuario">Crear Usuario</a>
+
+
+
+    </section>
+
+    <?php
+    require 'footer.php';
+    ?>
+
+
+
+</body>
+
+</html>
